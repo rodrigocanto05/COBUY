8f6038dc808d2847110d6f0677185f9cf457218c
Rodrigo Canto <email>
2025-10-31 11:26:52 +0000

    jjdjd

--- cobuybackend/src/main/java/com/cobuy/cobuybackend/controller/GroupController.java
+++ cobuybackend/src/main/java/com/cobuy/cobuybackend/controller/GroupController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/groups")
+@RequestMapping("/groups") //ola
 public class GroupController {
 
     private final GroupRepository groupRepository;
